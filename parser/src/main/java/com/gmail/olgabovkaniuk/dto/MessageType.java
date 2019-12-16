fcd5227492573e65ca98ee24d83eb35d8d044345
Olga Bovkaniuk <olgabovkaniuk@gmail.com>
2019-12-16 19:36:32 +0300

    cleanup code

--- parser/src/main/java/com/gmail/olgabovkaniuk/dto/MessageType.java
+++ parser/src/main/java/com/gmail/olgabovkaniuk/dto/MessageType.java
@@ -1,16 +1,20 @@
 package com.gmail.olgabovkaniuk.dto;
 
+import java.util.logging.Logger;
+
 public enum MessageType {
     INFO,
     WARNING,
     ERROR;
+
+    private static final Logger log = Logger.getLogger(MessageType.class.getName());
 
     public static MessageType getMessageType(String messageType) {
         try {
             return MessageType.valueOf(messageType.toUpperCase());
         } catch (IllegalArgumentException e) {
             System.out.println("messageType has not been found: " + messageType);
-            e.printStackTrace();
+            log.severe(e.getMessage());
         }
         return null;
     }
